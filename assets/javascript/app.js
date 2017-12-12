18d17d24db3ad55f035122adec7d68cb6ec8b602
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2017-12-12 17:37:25 -0500

    pushn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
 // alert("Hello Beautiful, Your Javascript is all hooked up, enjoy your homework journey");
+
+// These are my global variables*********************
+
+Var btnStart;
+var timeRemain;
+var questionNum;
+var answerOne;
+var answerTwo;
+var answerThree;
+var answerFour;
+var answerCorrect;
+var answerWrong;
+var answerUn;
+
+// This is my code for when you click on the start button that makes it go to main game screen but doesn't work.  Also should hold a buch of code for everything happening on the main game screen but not even an alert yet.
+$("#btn btn-primary btn-lg").click {
+    alert( "Handler for .click() called." );
+  };
+
+
